50bad2f7b9e98a2ebf32a69dbcd9b01347ba318c
Xiaolin van Heemst-Song <xiaolin.van.heemst-song@rabobank.nl>
2018-07-15 12:04:01 +0200

    Phonecat step 14 Animation

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
 // Define the `phonecatApp` module
-angular.module('phonecatApp', ['ngRoute', 'core','phoneDetail', 'phoneList']);
+angular.module('phonecatApp', ['ngAnimate','ngRoute', 'core','phoneDetail', 'phoneList']);
